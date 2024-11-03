82a006d6d94f4184ebca0ac3bd8e63c1111b38ff
Christopher Albert <albert@alumni.tugraz.at>
2024-11-03 17:55:18 +0000

    woergl

--- minipic/spline.py
+++ minipic/spline.py
@@ -3,7 +3,19 @@ import numpy as np
 from numba import njit
 
 @njit
-def single_bspline_hat(x, knots, degree, i):
+def periodic(fun, x, knots, degree, i):
+    n = len(knots) - 2*(1 + degree)
+    dx = 1.0/(n + 1 + degree)
+    if i > n:
+        y = x.copy()
+        y[x < (i-n)*dx] += 1.0
+        return fun(y, knots, degree, i)
+    else:
+        return fun(x, knots, degree, i)
+
+
+@njit
+def bspline_hat(x, knots, degree, i):
     """Generates a single B-spline basis function."""
     n_knots = len(knots)
     if degree == 0:
@@ -11,31 +23,46 @@ def single_bspline_hat(x, knots, degree, i):
     else:
         left = (x - knots[i]) / (knots[i + degree] - knots[i] + 1e-10)
         right = (knots[i + degree + 1] - x) / (knots[i + degree + 1] - knots[i + 1] + 1e-10)
-        return left * single_bspline_hat(x, knots, degree - 1, i) + \
-               right * single_bspline_hat(x, knots, degree - 1, i + 1)
+        return left * bspline_hat(x, knots, degree - 1, i) + \
+               right * bspline_hat(x, knots, degree - 1, i + 1)
+
 
 @njit
-def generate_periodic_bspline_basis(n, degree, x):
+def bspline_hat_derivative(x, knots, degree, i):
+    """Generates the derivative of a single B-spline basis function."""
+    n_knots = len(knots)
+    if degree == 0:
+        return np.where((x >= knots[i]) & (x < knots[i + 1]), 1.0, 0.0)
+    else:
+        left = 1.0 / (knots[i + degree] - knots[i] + 1e-10)
+        right = -1.0 / (knots[i + degree + 1] - knots[i + 1] + 1e-10)
+        return left * bspline_hat(x, knots, degree - 1, i) + \
+               right * bspline_hat(x, knots, degree - 1, i + 1)
+
+
+@njit
+def generate(fun, n, degree, x):
     n_basis = n + 1 + degree
     basis = np.zeros((len(x), n_basis))
 
     for i in range(n_basis):
-        evaluate_basis(n, degree, i, x, basis[:, i])
+        evaluate(fun, n, degree, i, x, basis[:, i])
 
     return basis
 
+
 @njit
-def evaluate_basis(n, degree, i, x, result):
+def init_knots(n, degree):
+    dx = 1.0/(n + 1 + degree)
+    return np.linspace(0.0, 1.0 + degree*dx, n + 2*(1 + degree))
+
+
+@njit
+def evaluate(fun, n, degree, i, x, result):
     """Evaluates the i-th B-spline basis function at the points x."""
     dx = 1.0/(n + 1 + degree)
-    knots = np.linspace(0.0, 1.0 + degree*dx, n + 2*(1 + degree))
+    knots = init_knots(n, degree)
     n_basis = len(knots) - degree - 1
     if i < 0 or i >= n_basis:
         result[:] = 0.0
-
-    if i > n:
-        y = x.copy()
-        y[x < (i-n)*dx] += 1.0
-        result[:] = single_bspline_hat(y, knots, degree, i)
-    else:
-        result[:] = single_bspline_hat(x, knots, degree, i)
+    result[:] = periodic(fun, x, knots, degree, i)
--- minipic/test_spline.py
+++ minipic/test_spline.py
@@ -1,11 +1,36 @@
-from spline import generate_periodic_bspline_basis
+from spline import (
+    generate,
+    bspline_hat,
+    bspline_hat_derivative,
+)
+
 
 def test_partition_unity():
     x = np.linspace(0, 1, 100)
     for degree in range(1, 7):
         for n in range(1, 30):
-            basis = generate_periodic_bspline_basis(n, degree, x)
-            assert(np.allclose(basis.sum(axis=1), 1.0))
+            basis = generate(bspline_hat, n, degree, x)
+            assert np.allclose(basis.sum(axis=1), 1.0)
+
+
+def test_derivatives():
+    x = np.linspace(0, 1, 100)
+    for degree in range(1, 7):
+        for n in range(1, 30):
+            basis_der = generate(bspline_hat_derivative, n, degree, x)
+            print(degree, n)
+            assert np.allclose(basis.sum(axis=1), 0.0)
 
 if __name__ == "__main__":
     test_partition_unity()
+    #test_derivatives()
+
+    x = np.linspace(0, 1, 100)
+    degree = 1
+    n = 1
+    basis_der = generate(bspline_hat_derivative, n, degree, x)
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.plot(x, basis_der)
+    plt.figure()
+    plt.plot(x, np.sum(basis_der, axis=1))
