8cced56061b683708ffd565f90877cc66c124893
Christopher Albert <albert@alumni.tugraz.at>
2024-11-03 12:17:21 +0000

    splini

--- minipic/spline.py
+++ minipic/spline.py
@@ -16,25 +16,21 @@ def single_bspline_hat(x, knots, degree, i):
 
 def generate_periodic_bspline_basis(n, degree, x):
     """Generates the set of periodic B-spline basis functions."""
-    # Create n+1 equally spaced knots (n periodic functions)
-    knots = np.linspace(0, 1, n + 1 + degree)
-
-    # Number of basis functions
+    dx = 1.0/(n + 1 + degree)
+    knots = np.arange(0, 1.0+3*dx, step=dx)
     n_basis = len(knots) - degree - 1
 
-    # Initialize the basis array
     basis = np.zeros((len(x), n_basis))
 
-    # Compute basis functions
     for i in range(n_basis):
         basis[:, i] = single_bspline_hat(x, knots, degree, i)
 
     return basis
 
 # Example usage:
-degree = 5
+degree = 3
 n = 11  # Number of basis functions
-x = np.linspace(0, 1, 100)
+x = np.linspace(0, 1.5, 1000)
 basis = generate_periodic_bspline_basis(n, degree, x)
 
 import matplotlib.pyplot as plt
