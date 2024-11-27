f5da95ec8f9f56f5aa23114b8e02b402de5bd9ac
Christopher Albert <albert@alumni.tugraz.at>
2024-11-27 19:37:00 +0100

    Clicky creates exams

--- clicky/create_exam.py
+++ clicky/create_exam.py
@@ -2,26 +2,92 @@ import sys
 import pyautogui
 from pathlib import Path
 from time import sleep
+from datetime import date
 
 base_path = Path('/Users/ert/Nextcloud/clicky')
 
 
 def main():
     lecture_name = sys.argv[1]
+    if len(sys.argv) > 2:
+        participant = sys.argv[2]
+    else:
+        participant = None
+    if len(sys.argv) > 3:
+        exam_date = sys.argv[3]
+    else:
+        exam_date = today()
+
     click_button('manage_exams')
     sleep(0.5)
-    click_button('create_exam')
-    sleep(0.5)
+    create_exam()
+    sleep(1.0)
     click_button('new_exam')
     sleep(0.5)
-    click_button('exam_lecture')
-    sleep(4)
-    type_text(lecture_name)
-
-
-def type_text(text):
-    pyautogui.write(text, interval=0.01)
+    select_lecture()
+    sleep(2.0)
+    select_all()
+    delete_text()
+    pyautogui.write(lecture_name + "*")
     pyautogui.press('enter')
+    sleep(1.0)
+    activate_lecture_radiobox()
+    sleep(0.1)
+    confirm_lecture()
+    sleep(0.5)
+    click_button('exam_date')
+    sleep(0.1)
+    pyautogui.write(exam_date)
+    pyautogui.press('tab', 4)
+    pyautogui.press('down', 4)
+    pyautogui.press('enter')
+    pyautogui.press('tab', 21)
+    pyautogui.press('enter')
+    sleep(0.5)
+    if(participant):
+        register_participant(participant)
+
+
+def register_participant(participant):
+    select_participants()
+    sleep(0.5)
+    click_button('register')
+    sleep(0.5)
+    pyautogui.write(participant)
+    pyautogui.press('enter')
+
+
+def select_participants():
+    pyautogui.press('tab', 2)
+    pyautogui.press('enter')
+
+
+def create_exam():
+    pyautogui.press('tab', 9)
+    pyautogui.press('enter')
+
+
+def select_lecture():
+    pyautogui.press('tab', 1)
+    pyautogui.press('enter')
+
+
+def confirm_lecture():
+    pyautogui.press('tab', 3)
+    pyautogui.press('enter')
+
+
+def activate_lecture_radiobox():
+    pyautogui.press('tab', 4)
+    pyautogui.press('space')
+
+
+def delete_text():
+    pyautogui.press('backspace')
+
+
+def select_all():
+    pyautogui.hotkey('command', 'a')
 
 
 def click_button(name):
@@ -38,6 +104,10 @@ def transform(pos):
 
 def get_center(rect):
     return (rect.left + rect.width*0.5, rect.top + rect.height*0.5)
+
+
+def today():
+    return date.today().strftime('%d.%m.%Y')
 
 
 if __name__ == '__main__':
