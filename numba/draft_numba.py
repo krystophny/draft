c87d2df447883763fa33b14259d1a7e916cb77b8
Christopher Albert <albert@alumni.tugraz.at>
2024-12-15 22:07:11 +0100

    numba

--- numba/draft_numba.py
+++ numba/draft_numba.py
@@ -1,23 +1,53 @@
 from numba import njit
 from numba.experimental import jitclass
 
-@jitclass
-class A:
-    def __init__(self):
-        pass
+class Base():
     def doit(self):
-        return 123
+        raise NotImplementedError
 
 @jitclass
-class B:
-    def __init__(self):
-        pass
-    def doit(self):
-        return 456.3
+class A(Base):
+    x: float
+    y: float
 
-@njit(debug=True)
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def doit(self):
+        return self.x + self.y
+
+
+@jitclass
+class B(Base):
+    s: str
+
+    def __init__(self, s: str):
+        self.s = s
+
+    def doit(self):
+        return self.s
+
+
+@njit
 def call_doit(obj):
     return obj.doit()
 
-print(call_doit(A()))
-print(call_doit(B()))
+
+def factory(kind: str, x: float, y: float):
+    if kind == 'A':
+        return A(x, y)
+    elif kind == 'B':
+        return B(f'{x} {y}')
+
+
+a = A(1.0, 2.0)
+b = B('hello')
+c = factory('A', 3.0, 4.0)
+d = factory('B', 5.0, 6.0)
+
+
+print(call_doit(a))
+print(call_doit(b))
+print(call_doit(c))
+print(call_doit(d))
