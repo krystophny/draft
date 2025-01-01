0d328e8360219b8d8032a93c3ba640bf99d2362c
Christopher Albert <albert@alumni.tugraz.at>
2025-01-01 12:06:11 +0100

    Golden record MARS

--- mars_golden_record/main.m
+++ mars_golden_record/main.m
@@ -1,13 +1,19 @@
-addpath('/var/tmp/ert/code/external/MARS/RZplot');
+CODE = getenv('CODE');
+DATA = getenv('DATA');
+addpath([CODE '/external/MARS/RZplot']);
 
 global Mac
-global SDIR 
-SDIR = '/var/tmp/ert/data/DEMO/MARS/MARSQ_OUTPUTS_100kAt_dBkinetic_NTVkinetic_NEO2profs_KEYTORQ_1/';
+global SDIR
+
+SDIR = [DATA '/DEMO/MARS/MARSQ_OUTPUTS_100kAt_dBkinetic_NTVkinetic_NEO2profs_KEYTORQ_1/'];
 
 Mac.mm_plot = [];
 Mac.edge = 1.0;
 Mac.core = 0.0;
 Mac.plot_VM = 0;
 Mac.plot_VS = 0;
+Mac.Norm = 1.0;
 
 MacConvBoozerCustom
+
+disp(['Mac.Norm = ' num2str(Mac.Norm)]);
