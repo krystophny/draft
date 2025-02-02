ec563f58c52c3f934c85d3ea66223d04845d0f8b
Christopher Albert <albert@alumni.tugraz.at>
2025-02-02 23:21:45 +0100

    Wire working

--- pauli/src/pauli/field.py
+++ pauli/src/pauli/field.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numba import njit
+from scipy.special import ellipk, ellipe
+from numba import jit, njit
 from pauli.util import legendre_p, assoc_legendre_p
 
 EPS = 1e-13
@@ -7,17 +8,16 @@ mu0 = 4e-7 * np.pi
 
 # Fields are normalized to 1A*mu0/2pi
 
+
 @njit
 def bfield_wire(X, Y, Z, B):
     Bcyl = np.zeros(3)
-    R = np.sqrt(X**2 + Y**2)
-    cosphi = X / max(R, EPS)
-    sinphi = Y / max(R, EPS)
-    phi = np.arctan2(Y, X)
-    bfield_wire_cyl(R, phi, Z, Bcyl)
-    B[0] = Bcyl[0] * cosphi - Bcyl[1] * sinphi
-    B[1] = Bcyl[0] * sinphi + Bcyl[1] * cosphi
-    B[2] = Bcyl[2]
+    cyl_to_cart(bfield_wire_cyl, X, Y, Z, B)
+
+
+def bfield_circle(X, Y, Z, B):
+    Bcyl = np.zeros(3)
+    cyl_to_cart(bfield_circle_cyl, X, Y, Z, B)
 
 
 @njit
@@ -34,30 +34,74 @@ def bfield_wire_cyl(R, phi, Z, B):
     B[2] = 0.0
 
 
-@njit
 def bfield_circle_cyl(R, phi, Z, B):
+    """
+    Calculate the magnetic field of a circular loop in cylindrical coordinates.
+    http://plasmalab.pbworks.com/w/file/fetch/109941181/Bfield-calcs.pdf
+    https://tiggerntatie.github.io/emagnet-py/offaxis/off_axis_loop.html
+    """
+
+    a = 1.0
+    r = max(np.sqrt(R**2 + Z**2), EPS)
+
+    alpha = r / a
+    beta = Z / a
+    gamma = Z / r
+    Q = (1 + alpha) ** 2 + beta**2
+    k2 = 4 * alpha / Q
+
+    K = ellipk(k2)
+    E = ellipe(k2)
+
+    B[0] = (
+        gamma
+        / (np.pi * np.sqrt(Q))
+        * (E * (1 + alpha**2 + beta**2) / (Q - 4 * alpha) - K)
+    )
+    B[2] = (
+        1.0
+        / (np.pi * np.sqrt(Q))
+        * (E * (1 - alpha**2 - beta**2) / (Q - 4 * alpha) + K)
+    )
+
+
+@njit
+def bfield_circle_cyl2(R, phi, Z, B):
     """
     Calculate the magnetic field of a circular loop in cylindrical coordinates.
     See https://farside.ph.utexas.edu/teaching/jk1/Electromagnetism/node52.html
     """
-    LMAX = 32
+    LMAX = 128
     B[:] = 0.0
 
-    r = np.sqrt(R**2 + Z**2)
+    r = max(np.sqrt(R**2 + Z**2), EPS)
     costh = Z / r
     sinth = R / r
     for l in np.arange(1, LMAX, 2):
         Pl10 = assoc_legendre_p(l, 1, 0.0)
         Br = Pl10 * legendre_p(l, costh)
-        Bth = Pl10 * assoc_legendre_p(l, 1, costh) / l
+        Bth = Pl10 * assoc_legendre_p(l, 1, costh)
         if r < 1:
             Br *= r ** (l - 1.0)
             Bth *= r ** (l - 1.0) / l
         else:
             Br *= r ** (-l - 2.0)
-            Bth *= r ** (-l - 2.0) / (l + 1.0)
+            Bth *= -(r ** (-l - 2.0)) / (l + 1.0)
 
         B[0] += Br * sinth + Bth * costh
         B[2] += Br * costh - Bth * sinth
 
     B[:] = -np.pi * B[:]
+
+
+def cyl_to_cart(fun, X, Y, Z, B):
+    Bcyl = np.zeros(3)
+    R = np.sqrt(X**2 + Y**2)
+    phi = np.arctan2(Y, X)
+    fun(R, phi, Z, Bcyl)
+
+    cosphi = X / max(R, EPS)
+    sinphi = Y / max(R, EPS)
+    B[0] = Bcyl[0] * cosphi - Bcyl[1] * sinphi
+    B[1] = Bcyl[0] * sinphi + Bcyl[1] * cosphi
+    B[2] = Bcyl[2]
--- pauli/tests/test_field.py
+++ pauli/tests/test_field.py
@@ -3,9 +3,9 @@ import pytest
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from numba import njit
+from numba import jit
 
-from pauli.field import bfield_wire
+from pauli.field import bfield_wire, bfield_circle
 
 
 def test_plot(tmp_path):
@@ -17,10 +17,38 @@ def test_plot(tmp_path):
     assert plot_path.exists()
 
 
-def test_bfield_wire(tmp_path):
+# def test_bfield_wire(tmp_path):
+#    plot_field(bfield_wire, tmp_path)
+
+
+def test_bfield_circle(tmp_path):
+    plot_field_xz(bfield_circle, tmp_path)
+
+
+def plot_field_xz(fun, tmp_path):
+    nx = 50
+    nz = 50
+
+    x = np.linspace(-1.5, 1.5, nx)
+    z = np.linspace(-.5, .5, nz)
+
+    X, Z = np.meshgrid(x, z)
+
+    B = np.zeros((3, nx, nz))
+    apply_xz(fun, X, Z, B)
+    b = B / np.sqrt(np.sum(B**2, axis=0))
+
+    plt.streamplot(X, Z, B[0], B[2])
+    plot_path = tmp_path / f"test_{fun.__name__}.png"
+    plt.savefig(plot_path)
+    plt.close()
+    print("Plot saved to", plot_path)
+
+
+def plot_field_3d(fun, tmp_path):
     nx = 10
     ny = 10
-    nz = 2
+    nz = 10
 
     x = np.linspace(-2, 2, nx)
     y = np.linspace(-2, 2, ny)
@@ -29,19 +57,24 @@ def test_bfield_wire(tmp_path):
     X, Y, Z = np.meshgrid(x, y, z, indexing="ij")
 
     B = np.zeros((3, nx, ny, nz))
-    apply_3d(bfield_wire, X, Y, Z, B)
+    apply_3d(fun, X, Y, Z, B)
     b = B / np.sqrt(np.sum(B**2, axis=0))
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     ax.quiver(X, Y, Z, b[0], b[1], b[2])
-    plot_path = tmp_path / "test_bfield_wire.png"
+    plot_path = tmp_path / f"test_{fun.__name__}.png"
     plt.savefig(plot_path)
     plt.close()
     print("Plot saved to", plot_path)
 
 
+def apply_xz(fun, X, Z, B):
+    nx, nz = X.shape
+    for i in range(nx):
+        for k in range(nz):
+            fun(X[i, k], 0.0, Z[i, k], B[:, i, k])
 
-@njit
+
 def apply_3d(fun, X, Y, Z, B):
     nx, ny, nz = X.shape
     for i in range(nx):
