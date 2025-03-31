612651f81269bc3228626de12e5a4f9ca15faac8
Christopher Albert <albert@alumni.tugraz.at>
2025-03-31 21:43:27 +0200

    single file

--- dispatch/without_data/Fortran/dispatch_switch.f90
+++ dispatch/without_data/Fortran/dispatch_switch.f90
@@ -1,3 +1,108 @@
+module dispatch_switch_module
+    implicit none
+
+    ! Define an enum to represent the type of data stored
+    enum, bind(c)
+        enumerator :: TYPE_INT = 1, TYPE_FLOAT, TYPE_DOUBLE, TYPE_UNKNOWN
+    end enum
+
+    ! Define a type to hold different types of data
+    type :: Data
+        real(8) :: double_value
+    end type Data
+
+    ! Define a struct that holds the data and the type of data it contains
+    type :: Value
+        integer :: type
+        type(Data) :: dat
+    end type Value
+
+contains
+
+    ! Function to perform arithmetic on an int
+    function process_int(val) result(result_value)
+        real(8), intent(in) :: val
+        real(8) :: result_value
+        result_value = val + 10.0
+    end function process_int
+
+    ! Function to perform arithmetic on a float
+    function process_float(val) result(result_value)
+        real(8), intent(in) :: val
+        real(8) :: result_value
+        result_value = val * 2.0
+    end function process_float
+
+    ! Function to perform arithmetic on a double
+    function process_double(val) result(result_value)
+        real(8), intent(in) :: val
+        real(8) :: result_value
+        result_value = val / 2.0
+    end function process_double
+
+    ! Function to process the value based on its type
+    subroutine process_value(val)
+        type(Value), intent(inout) :: val
+        select case (val%type)
+        case (TYPE_INT)
+            val%dat%double_value = process_int(val%dat%double_value)
+        case (TYPE_FLOAT)
+            val%dat%double_value = process_float(val%dat%double_value)
+        case (TYPE_DOUBLE)
+            val%dat%double_value = process_double(val%dat%double_value)
+        case default
+            print *, "Unsupported type"
+        end select
+    end subroutine process_value
+
+    ! Function to read the data type from the config file
+    function read_config(filename) result(type)
+        character(len=*), intent(in) :: filename
+        integer :: type
+        character(len=16) :: type_str
+        integer :: iostat
+
+        open(unit=10, file=filename, status='old', action='read', iostat=iostat)
+        if (iostat /= 0) then
+            print *, "Failed to open config file"
+            type = TYPE_UNKNOWN
+            return
+        end if
+
+        read(10, *, iostat=iostat) type_str
+        if (iostat /= 0) then
+            print *, "Failed to read type from config file"
+            type = TYPE_UNKNOWN
+            close(10)
+            return
+        end if
+
+        close(10)
+
+        select case (trim(type_str))
+        case ("int")
+            type = TYPE_INT
+        case ("float")
+            type = TYPE_FLOAT
+        case ("double")
+            type = TYPE_DOUBLE
+        case default
+            type = TYPE_UNKNOWN
+        end select
+    end function read_config
+
+    subroutine main_loop(val, iterations)
+        type(Value), intent(inout) :: val
+        integer, intent(in) :: iterations
+
+        integer :: i
+
+        do i = 1, iterations
+            call process_value(val)
+        end do
+    end subroutine main_loop
+
+end module dispatch_switch_module
 
 program main
     use dispatch_switch_module
