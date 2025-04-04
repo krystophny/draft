125200da59c51a0714e5fc226cca095461919c3c
Christopher Albert <albert@tugraz.at>
2025-04-04 06:56:12 +0200

    computor setup

--- computor/setup_computor.sh
+++ computor/setup_computor.sh
@@ -1,1 +1,17 @@
+#!/usr/bin/env bash
 
+apt update
+apt install -y \
+    git \
+    docker.io \
+    docker-compose
+
+mkdir -p ~/computor
+cd ~/computor
+
+git clone \
+    git@gitlab.tugraz.at:codeability/development/execution-backend-stub.git \
+    execution-backend
+
+cd execution-backend
+docker-compose up -d
